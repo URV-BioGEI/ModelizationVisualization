51867c5b3597430781db72d03ed7640bd4895e0f
Aleix Mariné Tena <aleix.marine@estudiants.urv.cat>
2019-09-25 14:47:29 +0200

    Exercise 1: Implemented Bresenham Algorithm and deleted some code from the teacher to start testing mine

--- src/Ex1/src/cat/urv/deim/miv/laboratoris/Laboratori1.java
+++ src/Ex1/src/cat/urv/deim/miv/laboratoris/Laboratori1.java
@@ -11,7 +11,8 @@ public class Laboratori1 extends Application {
 	}
 
 	public void paint() {
-		int width = getPanelWidth();
+
+		/*int width = getPanelWidth();
 		int height = getPanelHeight();
 		
 		int nlines = 10;
@@ -23,19 +24,81 @@ public class Laboratori1 extends Application {
 			drawLine(0, curHeight, width - curWidth, 0);
 			curWidth = width * i / nlines;
 			curHeight = height * i / nlines;
-		}
+		} */
 
 	}
 
 	// Practica 1, implementa defineDrawLine
 	// Hint: Pots utilitzar l'algorisme de Bresenham
 	// Hint: Per dibuixar un punt a la pantalla utilitza el metode drawPoint(x, y);
-	
-	// Inici codi de l'alumne
-	
-	public void defineDrawLine(int x1, int y1, int x2, int y2) {
-		// TODO: has de ficar aqui el codi!
 
+	/**
+	 * Bresenham Algorithm to draw lines
+	 * @author Aleix Mariné Tena
+ 	 * @param x1 x coordinate origin point
+	 * @param y1 y coordinate origin point
+	 * @param x2 x coordinate destination point
+	 * @param y2 y coordinate destination point
+	 */
+	public void defineDrawLine(int x1, int y1, int x2, int y2) {
+		// displacement for each axis
+		int dY = y2 - y1;
+		int dX = x2 - x1;
+
+		// Obtain dY and dX as module
+		// Locate line in one of the four quadrants
+		int IncYi, IncXi;
+		if (dY >= 0) IncYi = 1;
+		else
+		{
+			dY = -dY;
+			IncYi = -1;
+		}
+
+		if (dX >= 0) IncXi = 1;
+		else
+		{
+			dX = -dX;
+			IncXi = -1;
+		}
+
+		// Locate which axis needs to be increased by 1 all the time
+		int IncXr, IncYr;
+		if (dX >= dY)
+		{
+			IncYr = 0;
+			IncXr = IncXi;
+		}
+		else
+		{
+			IncXr = 0;
+			IncYr = IncYi;
+
+			// When dY > dX we swap them to only use one loop
+			int tmp = dX;
+			dX = dY;
+			dY = tmp;
+		}
+
+		// Initialize values to handle decimal error with integers
+		int X = x1, Y = y1, avR = 2 * dY, av = avR - dX, avI = av - dX;
+
+		// Line draw
+		do {
+			drawPoint(X, Y);
+			if (av >= 0)
+			{
+				X += IncXi;
+				Y += IncYi;
+				av += avI;
+			}
+			else
+			{
+				X += IncXr;
+				Y += IncYr;
+				av += avR;
+			}
+		} while (X != x2);
 	}
 
 	// Fi codi de l'alumne
